48fd4a6fdc8db9bf56464307d32fe79f574988a9
mark <marktestzxc@gmail.com>
2025-07-06 02:05:08 +0800

    update firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -1,24 +1,20 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
-import { getAnalytics } from 'firebase/analytics';
 import { getAuth } from 'firebase/auth';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from 'firebase/firestore';
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: 'AIzaSyAiFXamP_tfQfWRS2RF0fTcw_RV95rbcW0',
-  authDomain: 'tk-raffle-data.firebaseapp.com',
-  projectId: 'tk-raffle-data',
-  storageBucket: 'tk-raffle-data.firebasestorage.app',
-  messagingSenderId: '451018844608',
-  appId: '1:451018844608:web:08e75f2ba36dcbcd35c025',
-  measurementId: 'G-X3L3GNF25W',
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 
 export const auth = getAuth(app);
+export const db = getFirestore(app);
